650bea8c24762b2d13a4849ebfe986cd9df0db99
daniilvarlamov <varlamov.dan2013@mail.ru>
2023-07-09 18:17:14 +0300

    1

--- практика/1.py
+++ практика/1.py
@@ -2,13 +2,17 @@ from sqlalchemy import create_engine, Column, Integer, String, Date,ForeignKey, 
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.postgresql import JSONB    
+import random
+import string
+import names
+from datetime import datetime, timedelta
 import test
 
-
-engine = create_engine('postgresql://postgres:1qaz!QAZ@localhost:5432/Bank')
+engine,bind=test.create_con('postgresql://postgres:1qaz!QAZ@localhost:5432/Bank')
 
 Session=sessionmaker(bind=engine)
 session=Session()
-a=session.query(test.clients).all()
-for item in a:
-    print(item.id)
+result=session.query(test.clients).all()
+for row in result:
+    print(row.id,row.address)
--- практика/test.py
+++ практика/test.py
@@ -9,33 +9,20 @@ import names
 from datetime import datetime, timedelta
 
 
-#генерация
-def generate_random_string(length):
-    letters = string.ascii_letters
-    return ''.join(random.choice(letters) for i in range(length))
+def create_con(con_str):
+    engine = create_engine(con_str)
+    Base=declarative_base()
+    return engine,Base
 
-def generate_random_date(start_date, end_date):
-    days = (end_date - start_date).days
-    random_days = random.randint(0, days)
-    random_date = (start_date + timedelta(days=random_days)).__str__()
-    return random_date
-
-def generate_random_phone_number():
-    return ''.join(random.choices(string.digits, k=10))
-
-def generate_random_email():
-    return f"{generate_random_string(5)}@{generate_random_string(5)}.com"
+engine = create_engine('postgresql://postgres:1qaz!QAZ@localhost:5432/Bank')
+Base=declarative_base()
 
 
 #создание модели данных
-engine = create_engine('postgresql://postgres:1qaz!QAZ@localhost:5432/Bank')
-
-Base=declarative_base()
-
 class clients(Base):
     __tablename__='clients'
     id=Column(Integer, primary_key=True,autoincrement=True)
-    data=Column(JSONB)
+    # data=Column(JSONB)
     firstname=Column(String)
     lastname=Column(String)
     dateofbirth=Column(Date)
@@ -46,6 +33,7 @@ class clients(Base):
     registrationdate=Column(Date)
     bankaccount=relationship('bankaccounts')
 
+
 class bankaccounts(Base):
     __tablename__='bankaccounts'
     id=Column(Integer,primary_key=True, autoincrement=True)
@@ -55,7 +43,6 @@ class bankaccounts(Base):
     creationdate=Column(Date)
     balance=Column(Numeric)
     
-
 
 class transactions(Base):
     __tablename__='transactions'
@@ -75,10 +62,23 @@ class suspensiontransactions(Base):
     transaction_date=Column(DateTime)
     Comment=Column(String)
 
+#генерация
+def generate_random_string(length):
+    letters = string.ascii_letters
+    return ''.join(random.choice(letters) for i in range(length))
 
-Base.metadata.create_all(engine)
-Session=sessionmaker(bind=engine)
-session=Session()
+def generate_random_date(start_date, end_date):
+    days = (end_date - start_date).days
+    random_days = random.randint(0, days)
+    random_date = (start_date + timedelta(days=random_days)).__str__()
+    return random_date
+
+def generate_random_phone_number():
+    return ''.join(random.choices(string.digits, k=10))
+
+def generate_random_email():
+    return f"{generate_random_string(5)}@{generate_random_string(5)}.com"
+
 def create_clients_data(session,count):
     for i in range(count):
         user=clients(firstname=names.get_first_name(),lastname=names.get_last_name(),dateofbirth = generate_random_date(datetime(1980, 1, 1), datetime(2000, 12, 31)),
@@ -91,14 +91,26 @@ def create_acconts_data(session):
     result=session.query(clients).all()
     ids=[row[0] for row in result]
     for i in range(max(ids)):
-        account=bankaccounts(accountnumber = generate_random_string(10),
-                                 accounttype = random.choice(["дебетовый", "кредитный"]),
-                                 creationdate = generate_random_date(datetime(2010, 1, 1), datetime.now()),
-                                 balance = random.randint(1000, 10000))
+        account=bankaccounts(clientid=i,
+                             accountnumber = generate_random_string(10),
+                             accounttype = random.choice(["дебетовый", "кредитный"]),
+                             creationdate = generate_random_date(datetime(2010, 1, 1), datetime.now()),
+                             balance = random.randint(1000, 10000))
         session.add(account)
     session.commit()
+    
 
 def create_transactions_data(session):
-    pass
+    result=session.query(bankaccounts).all()
+    ids=[row[0] for row in result]
+    clientids=session.query(clients).all()
+    for i in range(max(ids)):
+        transaction=transactions(senderaccountid=random.choice())
 
 
+if __name__=='__main__':
+    Base.metadata.create_all(bind=engine)
+    Session=sessionmaker(bind=engine)
+    session=Session()
+    create_clients_data(session,10)
+    create_acconts_data(session)
